8de9f1565066138c6e4a48e7067e4eb5789df970
god <naughty7878@163.com>
2025-05-11 22:22:48 +0800

    新增超时过滤器

---
+++ mall-gateway/src/main/java/com/hd/mall/gateway/filter/TimeoutFilter.java
@@ -0,0 +1,41 @@
+package com.hd.mall.gateway.filter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@Component
+public class TimeoutFilter implements GlobalFilter, Ordered {
+
+    // 设置超时时间为60秒
+    @Value("${route.gateway.expiry:60}") // 缓存过期时间，默认 60 分钟
+    private long gatewayExpiry;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return chain.filter(exchange)
+                .timeout(Duration.ofSeconds(gatewayExpiry))
+                .onErrorResume(throwable -> {
+                    if (throwable instanceof java.util.concurrent.TimeoutException) {
+                        ServerHttpResponse response = exchange.getResponse();
+                        response.setStatusCode(HttpStatus.GATEWAY_TIMEOUT);
+                        return response.setComplete();
+                    }
+                    return Mono.error(throwable);
+                });
+    }
+
+    @Override
+    public int getOrder() {
+        return -999; // 设置过滤器的执行顺序
+    }
+
+}
